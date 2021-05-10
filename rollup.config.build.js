d18a5f7aa17959ba116b6fb48836e41b5afc4331
Nosov Konstantin <nosov@nodeart.io>
2021-05-10 18:07:45 +0300

    use scss instead of sass, generate sourcemaps

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,15 +1,17 @@
 import { terser } from 'rollup-plugin-terser';
 import resolve from "@rollup/plugin-node-resolve"
+import scss from 'rollup-plugin-scss'
 
 export default {
   input: './src/js/Game.js',
   plugins: [
     resolve(),
+    scss({sourceMap: true, sass: require('sass')}),
     terser(),
   ],
   output: {
       format: 'iife',
-      file: './static/assets/js/cube.js',
+      file: './export/cube.js',
       indent: '\t',
       sourcemap: true,
   },
