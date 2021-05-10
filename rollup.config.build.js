23332da923cf03b9f3d35a0b7cee20e7cf68973c
Nosov Konstantin <nosov@nodeart.io>
2021-05-10 19:44:58 +0300

    add autoprefixer (not actually needed, but why not)

--- rollup.config.build.js
+++ rollup.config.build.js
@@ -1,12 +1,17 @@
 import { terser } from 'rollup-plugin-terser';
 import resolve from "@rollup/plugin-node-resolve"
 import scss from 'rollup-plugin-scss'
+import postcss from "postcss";
+import autoprefixer from "autoprefixer";
 
 export default {
   input: './src/js/Game.js',
   plugins: [
     resolve(),
-    scss({sourceMap: true, sass: require('sass')}),
+    scss({sourceMap: true, sass: require('sass'),
+      processor: css =>
+        postcss([autoprefixer()]),
+    }),
     terser(),
   ],
   output: {
